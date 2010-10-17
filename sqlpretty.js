905f4d0bb34443469ea291b486a836de32d05b2a
juliannoble <julian@precisium.com.au>
2010-10-17 13:42:54 +0000

    Add an id attribute (based on SQL_PRETTY_LINKNAME) to each div.sqlpretty.
    Add SQLPRETTY global object and refactor the main hide/show code into a function at SQLPRETTY.init
    init optionally takes the id of the div.sqlpretty to which we want to setup click binding for hide/show.

--- sqlpretty.js
+++ sqlpretty.js
@@ -1,21 +1,35 @@
+if (typeof SQLPRETTY == "undefined") {
+    var SQLPRETTY = {};
+}	
 jQuery(document).ready(function() {
-   jQuery('.sqlpretty-result').hide();
-   jQuery('.sqlpretty-hide-show').each(function(idx) {
-       var lnkname = jQuery(this).html()
-       jQuery(this).html('Show ' + lnkname);
-       jQuery(this).click(function() {
-           var debugdiv = jQuery(this).parent().parent();
-           var resdiv = debugdiv.find('.sqlpretty-result');
-           var lnk = debugdiv.find('.sqlpretty-hide-show');
-           if (resdiv.is(':visible')) {
-               lnk.html('Show ' + lnkname);
-               resdiv.hide();
-           } else {
-               lnk.html('Hide ' + lnkname);
-               resdiv.show();
-           }
-           return false;
-       });
-       jQuery(this).parent().css('visibility','visible');
-   });
+    SQLPRETTY.init = function() {
+        var sqlid = '';
+        if (arguments[0]) {
+            sqlid = arguments[0];
+            sqlid.replace('#','');
+            sqlid = '#' + sqlid + ' ';
+        }
+        jQuery(sqlid + '.sqlpretty-result').hide();
+        
+        jQuery(sqlid + '.sqlpretty-hide-show').each(function(idx) {
+           var lnkname = jQuery(this).html()
+           jQuery(this).html('Show ' + lnkname);
+           jQuery(this).unbind('click.sqlpretty');
+           jQuery(this).bind('click.sqlpretty',function() {
+               var debugdiv = jQuery(this).parent().parent();
+               var resdiv = debugdiv.find('.sqlpretty-result');
+               var lnk = debugdiv.find('.sqlpretty-hide-show');
+               if (resdiv.is(':visible')) {
+                   lnk.html('Show ' + lnkname);
+                   resdiv.hide();
+               } else {
+                   lnk.html('Hide ' + lnkname);
+                   resdiv.show();
+               }
+               return false;
+           });
+           jQuery(this).parent().css('visibility','visible');
+        });
+    };
+    SQLPRETTY.init();
 });
